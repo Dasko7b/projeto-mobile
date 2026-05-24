465ec2b55dc49a021ab77aa2d3d56ed56e1bb969
Thiago Cordeiro <thiagocordeiro2512@gmail.com>
2026-05-24 16:12:49 -0300

    fix: corrige tabs e icons grupos e perfils

--- src/navigations/AppTabs.tsx
+++ src/navigations/AppTabs.tsx
@@ -1,4 +1,10 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import {
+    Users,
+    User
+} from "lucide-react-native";
+
 import { GroupStack } from "./GroupStack";
 import ProfileScreen from "../screens/profile/ProfileScreen";
 
@@ -7,18 +13,65 @@ const Tab = createBottomTabNavigator();
 export function AppTabs() {
 
     return (
-        <Tab.Navigator>
+
+        <Tab.Navigator
+
+            screenOptions={({ route }) => ({
+
+                headerShown: false,
+
+                tabBarActiveTintColor: "#2563eb",
+
+                tabBarInactiveTintColor: "#777",
+
+                tabBarIcon: ({
+                    color,
+                    size
+                }) => {
+
+                    if (route.name === "Grupos") {
+
+                        return (
+
+                            <Users
+                                color={color}
+                                size={size}
+                            />
+
+                        );
+
+                    }
+
+                    if (route.name === "Perfil") {
+
+                        return (
+
+                            <User
+                                color={color}
+                                size={size}
+                            />
+
+                        );
+
+                    }
+
+                }
+
+            })}
+
+        >
 
             <Tab.Screen
-                name="Groups"
+                name="Grupos"
                 component={GroupStack}
             />
 
             <Tab.Screen
-                name="Profile"
+                name="Perfil"
                 component={ProfileScreen}
             />
 
         </Tab.Navigator>
+
     );
 }
